9e03a095d6d66a2a8d9cbb936950d8e0e19d3156
Petar Dimitrov <lbmikesmith@gmail.com>
2016-09-27 22:00:26 +0300

    Delete 10-AddCheckConstraint.sql

--- 10-AddCheckConstraint.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE Users
-ADD CHECK (DATALENGTH(Password) >= 5)
-
---With this you can name your Constraint, while the version above generates a random constraint name
-ALTER TABLE Users
-ADD CONSTRAINT MinLengthConstraint CHECK (DATALENGTH(Password) >= 5)
