dc3be0b3a45ce146ba36dedd1ceabd62de0d13a2
Petar Dimitrov <lbmikesmith@gmail.com>
2016-09-28 12:07:18 +0300

    Update 10-AddCheckConstraint.sql

--- DefinitionAndDataTypes/10-AddCheckConstraint.sql
+++ DefinitionAndDataTypes/10-AddCheckConstraint.sql
@@ -4,3 +4,6 @@ ADD CHECK (DATALENGTH(Password) >= 5)
 --With this you can name your Constraint, while the version above generates a random constraint name
 ALTER TABLE Users
 ADD CONSTRAINT MinLengthConstraint CHECK (DATALENGTH(Password) >= 5)
+
+--This SHOULD work properly
+ADD CONSTRAINT [PasswordMinSize] CHECK (LEN([Password]) >= 5)
