5cd5ca3ae005eae57e2d81d42a0c49aa2bddbdaa
Petar Dimitrov <lbmikesmith@gmail.com>
2016-09-29 10:58:19 +0300

    Add files via upload

---
+++ DefinitionAndDataTypes/20-BasicSelectAllFieldsAndOrderThem.sql
@@ -0,0 +1,6 @@
+SELECT * FROM [dbo].[Towns]
+ORDER BY [dbo].[Towns].Name ASC
+SELECT * FROM [dbo].[Departments]
+ORDER BY [dbo].[Departments].Name ASC
+SELECT * FROM [dbo].[Employees]
+ORDER BY [dbo].[Employees].Salary DESC
