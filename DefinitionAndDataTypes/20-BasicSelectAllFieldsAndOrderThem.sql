282a3155f608ae207ebd1d345035705350a6ae1e
Petar Dimitrov <lbmikesmith@gmail.com>
2016-09-29 11:00:17 +0300

    Second Version added

--- DefinitionAndDataTypes/20-BasicSelectAllFieldsAndOrderThem.sql
+++ DefinitionAndDataTypes/20-BasicSelectAllFieldsAndOrderThem.sql
@@ -4,3 +4,12 @@ SELECT * FROM [dbo].[Departments]
 ORDER BY [dbo].[Departments].Name ASC
 SELECT * FROM [dbo].[Employees]
 ORDER BY [dbo].[Employees].Salary DESC
+
+--This version also works by shortening the table name
+
+SELECT * FROM [dbo].[Towns] AS T
+ORDER BY T.Name ASC
+SELECT * FROM [dbo].[Departments] AS D
+ORDER BY D.Name ASC
+SELECT * FROM [dbo].[Employees] AS E
+ORDER BY E.Salary DESC
