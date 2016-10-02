b04cee163e99623059e59f16b788259e4e507628
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-02 21:22:23 +0300

    Update 21-IncreaseSalaries.sql

--- Basic-CRUD/21-IncreaseSalaries.sql
+++ Basic-CRUD/21-IncreaseSalaries.sql
@@ -4,3 +4,11 @@ WHERE DepartmentId = 1 OR DepartmentId = 2 OR DepartmentId = 4 OR DepartmentId =
 
 SELECT E.Salary
 FROM dbo.Employees AS E
+
+--Second version using decimal for calculations and IN instead of OR, OR, OR, OR
+UPDATE Employees
+SET Salary = Salary * 1.12
+WHERE DepartmentId IN (1, 2, 4, 11)
+
+SELECT E.Salary
+FROM dbo.Employees AS E
