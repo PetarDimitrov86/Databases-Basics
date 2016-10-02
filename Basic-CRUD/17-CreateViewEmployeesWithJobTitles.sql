a7cd623a0e20ee525c471122b1d75643400ab493
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-02 20:55:09 +0300

    Update 17-CreateViewEmployeesWithJobTitles.sql

--- Basic-CRUD/17-CreateViewEmployeesWithJobTitles.sql
+++ Basic-CRUD/17-CreateViewEmployeesWithJobTitles.sql
@@ -5,3 +5,8 @@ CASE
 	WHEN E.MiddleName IS NULL THEN E.FirstName + '  ' + E.LastName
 END
  AS "Full Name", E.JobTitle FROM dbo.Employees AS E
+ 
+ --Version two using CONCAT to replace all NULL with ' '
+CREATE VIEW V_EmployeeNameJobTitle AS
+SELECT CONCAT(E.FirstName, ' ', E.MiddleName, ' ', E.LastName) AS "Full Name", E.JobTitle 
+FROM dbo.Employees AS E
