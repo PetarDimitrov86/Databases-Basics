634a78ebeead25dc1479a062f7b2d876036d569d
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-02 20:03:32 +0300

    Update 09-FindNamesOfAllEmployeesBySalaryInRange.sql

--- Basic-CRUD/09-FindNamesOfAllEmployeesBySalaryInRange.sql
+++ Basic-CRUD/09-FindNamesOfAllEmployeesBySalaryInRange.sql
@@ -1,2 +1,6 @@
 SELECT E.FirstName, E.LastName, E.JobTitle FROM [dbo].Employees AS E
 WHERE E.Salary >= 20000 AND E.Salary <= 30000
+
+--Second version with  BETWEEN AND 
+SELECT E.FirstName, E.LastName, E.JobTitle FROM [dbo].Employees AS E
+WHERE E.Salary BETWEEN 20000 AND 30000
