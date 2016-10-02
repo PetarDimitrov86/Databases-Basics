c6d8996aa8d016b0a8bbe13683218a79cb0b7ef7
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-02 20:10:22 +0300

    Update 10-FindNamesOfAllEmployees.sql

--- Basic-CRUD/10-FindNamesOfAllEmployees.sql
+++ Basic-CRUD/10-FindNamesOfAllEmployees.sql
@@ -1,5 +1,9 @@
 SELECT E.FirstName + ' ' + E.MiddleName + ' ' + E.LastName AS "Full Name" FROM [dbo].Employees AS E
 WHERE E.Salary = 25000 
-OR E.Salary = 14000
-OR E.Salary = 12500
-OR E.Salary = 23600
+   OR E.Salary = 14000
+   OR E.Salary = 12500
+   OR E.Salary = 23600
+
+--Second version using IN
+SELECT E.FirstName + ' ' + E.MiddleName + ' ' + E.LastName AS "Full Name" FROM [dbo].Employees AS E
+WHERE E.Salary IN (25000, 14000, 12500, 23600)
