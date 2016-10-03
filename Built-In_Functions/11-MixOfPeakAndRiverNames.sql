c1240937517c9e847c7ba780255d37b9c81dbd03
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-03 13:17:28 +0300

    Update 11-MixOfPeakAndRiverNames.sql

--- Built-In_Functions/11-MixOfPeakAndRiverNames.sql
+++ Built-In_Functions/11-MixOfPeakAndRiverNames.sql
@@ -1,5 +1,5 @@
 SELECT P.PeakName, R.RiverName, LOWER(P.PeakName) + RIGHT(LOWER(R.RiverName), LEN(R.RiverName) - 1) AS "Mix"
 FROM Peaks AS P
 CROSS JOIN Rivers AS R
-WHERE RIGHT(LOWER(P.PeakName), 1) = LEFT(LOWER(R.RiverName), 1)
+WHERE RIGHT(P.PeakName, 1) = LEFT(R.RiverName, 1)
 ORDER BY Mix
