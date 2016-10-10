734542999ba678b9296d24376c727ceefd25b66c
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-10 19:00:48 +0300

    Update 19-CountriesWithoutAnyMountains.sql

--- TableRelations/19-CountriesWithoutAnyMountains.sql
+++ TableRelations/19-CountriesWithoutAnyMountains.sql
@@ -3,6 +3,7 @@ FROM [dbo].[Countries] AS c
 WHERE c.CountryCode NOT IN 
 (SELECT cc.CountryCode FROM [dbo].[MountainsCountries] AS cc)
 
+--Second version using LEFT JOIN (from denizz)
 SELECT COUNT(c.CountryCode) FROM [dbo].[Countries] AS c
 LEFT JOIN [dbo].[MountainsCountries] AS mc
 ON c.CountryCode = mc.CountryCode
