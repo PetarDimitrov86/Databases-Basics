5753cb8cab486295b08df5a60dd6995976441a2d
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-11 12:19:12 +0300

    Update 06-FindTownsStartingWith.sql

--- Built-In_Functions/06-FindTownsStartingWith.sql
+++ Built-In_Functions/06-FindTownsStartingWith.sql
@@ -9,3 +9,11 @@ ORDER BY T.Name ASC
 SELECT T.TownID, T.Name FROM [dbo].Towns AS T
 WHERE SUBSTRING(T.Name, 1, 1) IN ('M', 'K', 'B', 'E')
 ORDER BY T.Name ASC
+
+--Third version using WildCards
+SELECT T.TownID, T.Name FROM [dbo].Towns AS T
+WHERE T.Name LIKE 'M%'
+   OR T.Name LIKE 'K%'
+   OR T.Name LIKE 'B%'
+   OR T.Name LIKE 'E%'
+ORDER BY T.Name ASC
