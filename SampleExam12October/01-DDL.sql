59e05deac04e5bda97afd33abf2821f2bcdf5d3a
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-12 18:43:16 +0300

    Add files via upload

---
+++ SampleExam12October/01-DDL.sql
@@ -0,0 +1,54 @@
+CREATE TABLE DepositTypes(
+DepositTypeID INT PRIMARY KEY,
+Name VARCHAR(20)
+)
+
+CREATE TABLE Deposits(
+DepositID INT IDENTITY PRIMARY KEY,
+Amount DECIMAL(10, 2),
+StartDate DATE,
+EndDate DATE,
+DepositTypeID INT,
+CustomerID INT,
+CONSTRAINT FK_Deposits_DepositType FOREIGN KEY(DepositTypeID) REFERENCES DepositTypes(DepositTypeID),
+CONSTRAINT FK_Deposits_Customer FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+)
+
+CREATE TABLE EmployeesDeposits(
+EmployeeID INT UNIQUE,
+DepositID INT UNIQUE,
+CONSTRAINT PK_EmployeesDeposits PRIMARY KEY(EmployeeID, DepositID),
+CONSTRAINT FK_EmployeesDeposits_Employees FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
+CONSTRAINT FK_EmployeesDeposits_Deposits FOREIGN KEY (DepositID) REFERENCES Deposits(DepositID)
+)
+
+CREATE TABLE CreditHistory(
+CreditHistoryID INT PRIMARY KEY,
+Mark CHAR(1),
+StartDate DATE,
+EndDate DATE,
+CustomerID INT,
+CONSTRAINT FK_CreditHistory_CustomerID FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+)
+
+CREATE TABLE Payments(
+PaymentID INT PRIMARY KEY,
+Date DATE,
+Amount DECIMAL (10, 2),
+LoanID INT,
+CONSTRAINT FK_Payments_Loan FOREIGN KEY (LoanID) REFERENCES Loans(LoanID)
+)
+
+CREATE TABLE Users(
+UserID INT PRIMARY KEY,
+UserName VARCHAR(20),
+Password VARCHAR(20),
+CustomerID INT UNIQUE,
+CONSTRAINT FK_Users_Customers FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+)
+ALTER TABLE Employees
+ADD ManagerID INT
+
+ALTER TABLE Employees
+ADD CONSTRAINT FK_Employees_Employees FOREIGN KEY (ManagerID) REFERENCES Employees(EmployeeID)
+
