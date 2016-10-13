1da0111bbe96a0b78eca73a5d65c603535850e75
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-13 15:00:50 +0300

    Update 11-EmployeesWithoutProjects.sql

--- TableRelations/11-EmployeesWithoutProjects.sql
+++ TableRelations/11-EmployeesWithoutProjects.sql
@@ -1,3 +1,9 @@
 SELECT DISTINCT TOP 3 e.EmployeeID, e.FirstName FROM Employees AS e
 WHERE  e.EmployeeID NOT IN
 	(SELECT ep.EmployeeID FROM EmployeesProjects AS ep)
+	
+-- Second version using LEFT OUTER JOIN
+SELECT TOP 3 e.EmployeeID, e.FirstName FROM Employees AS e
+   LEFT OUTER JOIN EmployeesProjects AS ep
+   ON e.EmployeeID = ep.EmployeeID
+   WHERE ep.EmployeeID IS NULL
