2e3e032925dee25f1d8cc7b725b04284b8f1f168
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-15 14:14:59 +0300

    Update 03-Querying-10-OldestAccount.sql

--- SampleExam12October/03-Querying-10-OldestAccount.sql
+++ SampleExam12October/03-Querying-10-OldestAccount.sql
@@ -2,3 +2,9 @@ SELECT TOP 1 c.CustomerID, c.FirstName, a.StartDate FROM Customers AS c
 INNER JOIN Accounts AS a
 ON a.CustomerID = c.CustomerID
 ORDER BY a.StartDate ASC
+
+--Second version with Subquery
+SELECT c.CustomerID, c.FirstName, a.StartDate FROM Customers AS c
+INNER JOIN Accounts AS a
+ON a.CustomerID = c.CustomerID
+WHERE a.StartDate IN (SELECT MIN(a.StartDate) FROM Accounts AS a) 
